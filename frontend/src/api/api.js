f9a990d8e10f42743a5778f4625649831d4267d7
Abdelillah <derrarabdelillah55@gmail.com>
2025-07-26 17:21:47 +0100

    fix the bug of api link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,11 +5,11 @@ import { productsContext } from '../context/ProductsContext';
 const { backendUrl } = productsContext;
 
 export async function createUser(user) {
-    const response = await Axios.post(`${backendUrl}/users`, user);
+    const response = await Axios.post(`${backendUrl}/api/users`, user);
     return response.data;
 }
 
 export async function verifyUser(user) {
-    const response = await Axios.post(`${backendUrl}/users/login`, user);
+    const response = await Axios.post(`${backendUrl}/api/users/login`, user);
     return response.data;
 }
