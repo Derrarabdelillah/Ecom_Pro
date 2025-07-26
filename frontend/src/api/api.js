0be5f64334759f96e858a2a2a31b7f8e22165b62
Abdelillah <derrarabdelillah55@gmail.com>
2025-07-26 18:43:37 +0100

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,15 +1,15 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { productsContext } from '../context/ProductsContext';
 
 
 const { backendUrl } = productsContext;
 
 export async function createUser(user) {
-    const response = await Axios.post(`${backendUrl}/api/users`, user);
+    const response = await axios.post(`${backendUrl}/api/users`, user);
     return response.data;
 }
 
 export async function verifyUser(user) {
-    const response = await Axios.post(`${backendUrl}/api/users/login`, user);
+    const response = await axios.post(`${backendUrl}/api/users/login`, user);
     return response.data;
 }
