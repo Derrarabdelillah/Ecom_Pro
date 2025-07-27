6fb3cbb68b7020639eaf532609d88d6871a4746c
Abdelillah <derrarabdelillah55@gmail.com>
2025-07-27 14:19:20 +0100

    done

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { createOrder, getUserOrders, updateStatus, getAdminOrders } = require("../controllers/orderController");
-const { adminAuth } = require("../middleware/adminAuth");
-const authUser = require("../middleware/auth");
 
 
 // Public Route ( guest checkout )
 router.post('/placeOrder', createOrder);;
 
 // User Routes
-router.get('/myOrders', authUser, getUserOrders);
+router.get('/myOrders', getUserOrders);
 
 // Admin Routes
 router.put('/:orderId/status', updateStatus);
