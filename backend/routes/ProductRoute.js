375be04f1e7360815d694b9eb3215185a9d009de
Abdelillah <derrarabdelillah55@gmail.com>
2025-08-12 16:23:56 +0100

    done

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -14,7 +14,7 @@ const {
 const upload = require('../middleware/multer');
 const { adminAuth } = require('../middleware/adminAuth');
 
-router.post('/add', adminAuth, upload.fields([
+router.post('/add', upload.fields([
     {
         name: 'image1',
         maxCount: 1
@@ -33,10 +33,10 @@ router.post('/add', adminAuth, upload.fields([
     },
 ]), addProduct);
 
-router.get('/all', adminAuth, allProducts);
-router.delete('/remove/:id', adminAuth, removeProduct);
-router.get('/single/:id', adminAuth, singleProduct);
-router.put('/update/:id', adminAuth, upload.fields([
+router.get('/all', allProducts);
+router.delete('/remove/:id', removeProduct);
+router.get('/single/:id', singleProduct);
+router.put('/update/:id', upload.fields([
     {
         name: 'image1',
         maxCount: 1
