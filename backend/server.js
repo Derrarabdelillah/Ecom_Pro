a308b7257000682b8e0dd990a41df2f89b06360b
Abdelillah <derrarabdelillah55@gmail.com>
2026-06-13 18:26:45 +0100

    Feat: Add rate limiter for auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParse = require('cookie-parser');
+const rateLimit = require('express-rate-limit')
 
 // DB
 const connectDB = require('./config/connect');
@@ -18,6 +19,17 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // each IP can only make 5 requests to auth endpoints per windowMs
+    message: {
+      success: false,
+      message: 'Too many login attempts from this IP, please try again after 15 minutes'
+    },
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 app.use(cors(corsOptions));
 app.use(express.json()); // For JSON bodies
@@ -40,7 +52,7 @@ const PORT = process.env.PORT || 3000;
 
 
 // api endpoints
-app.use(UserRoute)
+app.use(UserRoute, authLimiter); // Apply rate limiter to auth routes
 app.use('/api/product', ProductRoute);
 app.use('/api/cart', CartRoute);
 app.use('/api/orders', OrderRoute);
